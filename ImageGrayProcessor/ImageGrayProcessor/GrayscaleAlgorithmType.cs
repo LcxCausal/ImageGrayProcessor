cfcfbf2f6a8f700a12dbe0e934599578e316738d
Lee Liu <leeliu@thinkgeo.com>
2018-11-22 23:49:29 +0800

    Implement Async methods and other alogorithms, including Maximun, Minimun, Average and WeightAverage.

--- ImageGrayProcessor/ImageGrayProcessor/GrayscaleAlgorithmType.cs
+++ ImageGrayProcessor/ImageGrayProcessor/GrayscaleAlgorithmType.cs
@@ -4,7 +4,8 @@
     {
         Component = 0,
         Maximum = 1,
-        Average = 2,
-        WeightedAverage = 4
+        Minimum = 2,
+        Average = 4,
+        WeightedAverage = 8
     }
 }
--- ImageGrayProcessor/ImageGrayProcessor/ImageGrayProcessor.cs
+++ ImageGrayProcessor/ImageGrayProcessor/ImageGrayProcessor.cs
@@ -1,7 +1,9 @@
 ﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ImageGrayProcessor
 {
@@ -21,6 +23,17 @@ namespace ImageGrayProcessor
 
         public string ImagePath { get; protected set; }
 
+        public static async Task<List<Image<Rgba32>>> ConvertImageToGrayscaleAsync(string imagePath, GrayscaleAlgorithmType grayscaleAlgorithmType)
+        {
+            if (string.IsNullOrEmpty(imagePath) || File.Exists(imagePath))
+            {
+                throw new ArgumentException();
+            }
+
+            Image<Rgba32> image = Image.Load(imagePath);
+            return await ConvertImageToGrayscaleAsync(image, grayscaleAlgorithmType);
+        }
+
         public static List<Image<Rgba32>> ConvertImageToGrayscale(string imagePath, GrayscaleAlgorithmType grayscaleAlgorithmType)
         {
             if (string.IsNullOrEmpty(imagePath) || File.Exists(imagePath))
@@ -32,26 +45,60 @@ namespace ImageGrayProcessor
             return ConvertImageToGrayscale(image, grayscaleAlgorithmType);
         }
 
-        public static List<Image<Rgba32>> ConvertImageToGrayscale(Image<Rgba32> image, GrayscaleAlgorithmType grayscaleAlgorithmType)
+        public static async Task<List<Image<Rgba32>>> ConvertImageToGrayscaleAsync(Image<Rgba32> image, GrayscaleAlgorithmType grayscaleAlgorithmType)
         {
             if (image == null) { return null; }
 
-            List<Image<Rgba32>> grayscaleImage = null;
+            List<Image<Rgba32>> grayscaleImages = new List<Image<Rgba32>>();
 
             switch (grayscaleAlgorithmType)
             {
                 case GrayscaleAlgorithmType.Component:
-                    grayscaleImage = ConvertImageFromComponentAlgorithm(image);
+                    grayscaleImages = await Task.FromResult(ConvertImageFromComponentAlgorithm(image));
                     break;
                 case GrayscaleAlgorithmType.Maximum:
+                    grayscaleImages = await Task.FromResult(ConvertImageFromMaximunAlogorithm(image));
+                    break;
+                case GrayscaleAlgorithmType.Minimum:
+                    grayscaleImages = await Task.FromResult(ConvertImageFromMinimunAlogorithm(image));
                     break;
                 case GrayscaleAlgorithmType.Average:
+                    grayscaleImages = await Task.FromResult(ConvertImageFromAverageAlogorithm(image));
                     break;
                 case GrayscaleAlgorithmType.WeightedAverage:
+                    grayscaleImages = await Task.FromResult(ConvertImageFromWeightedAverageAlogorithm(image));
                     break;
             }
 
-            return grayscaleImage;
+            return grayscaleImages;
+        }
+
+        public static List<Image<Rgba32>> ConvertImageToGrayscale(Image<Rgba32> image, GrayscaleAlgorithmType grayscaleAlgorithmType)
+        {
+            if (image == null) { return null; }
+
+            List<Image<Rgba32>> grayscaleImages = null;
+
+            switch (grayscaleAlgorithmType)
+            {
+                case GrayscaleAlgorithmType.Component:
+                    grayscaleImages = ConvertImageFromComponentAlgorithm(image);
+                    break;
+                case GrayscaleAlgorithmType.Maximum:
+                    grayscaleImages = ConvertImageFromMaximunAlogorithm(image);
+                    break;
+                case GrayscaleAlgorithmType.Minimum:
+                    grayscaleImages = ConvertImageFromMinimunAlogorithm(image);
+                    break;
+                case GrayscaleAlgorithmType.Average:
+                    grayscaleImages = ConvertImageFromAverageAlogorithm(image);
+                    break;
+                case GrayscaleAlgorithmType.WeightedAverage:
+                    grayscaleImages = ConvertImageFromWeightedAverageAlogorithm(image);
+                    break;
+            }
+
+            return grayscaleImages;
         }
 
         public List<Image<Rgba32>> ToGrayscale(GrayscaleAlgorithmType grayscaleAlgorithmType)
@@ -65,15 +112,13 @@ namespace ImageGrayProcessor
             return ConvertImageToGrayscale(image, grayscaleAlgorithmType);
         }
 
-        private static List<Image<Rgba32>> ConvertImageFromComponentAlgorithm(Image<Rgba32> image)
+        private static List<Image<Rgba32>> ConvertImageFromWeightedAverageAlogorithm(Image<Rgba32> image)
         {
             List<Image<Rgba32>> grayscaleImages = new List<Image<Rgba32>>();
             int imageWidth = image.Width;
             int imageHeight = image.Height;
 
-            Image<Rgba32> rComponentImage = new Image<Rgba32>(imageWidth, imageWidth);
-            Image<Rgba32> gComponentImage = new Image<Rgba32>(imageWidth, imageWidth);
-            Image<Rgba32> bComponentImage = new Image<Rgba32>(imageWidth, imageWidth);
+            Image<Rgba32> tmpImage = new Image<Rgba32>(imageWidth, imageHeight);
 
             for (int h = 0; h < imageHeight; h++)
             {
@@ -83,14 +128,110 @@ namespace ImageGrayProcessor
                     byte r = pixel.R;
                     byte g = pixel.G;
                     byte b = pixel.B;
+                    byte weightAvg = (byte)(r * 0.3 + g * 0.59 + b * 0.11);
+                    tmpImage[w, h] = new Rgba32(weightAvg, weightAvg, weightAvg);
+                }
+            }
 
-                    // R Component
+            grayscaleImages.Add(tmpImage);
+            return grayscaleImages;
+        }
+
+        private static List<Image<Rgba32>> ConvertImageFromAverageAlogorithm(Image<Rgba32> image)
+        {
+            List<Image<Rgba32>> grayscaleImages = new List<Image<Rgba32>>();
+            int imageWidth = image.Width;
+            int imageHeight = image.Height;
+
+            Image<Rgba32> tmpImage = new Image<Rgba32>(imageWidth, imageHeight);
+
+            for (int h = 0; h < imageHeight; h++)
+            {
+                for (int w = 0; w < imageWidth; w++)
+                {
+                    Rgba32 pixel = image[w, h];
+                    byte r = pixel.R;
+                    byte g = pixel.G;
+                    byte b = pixel.B;
+                    byte avg = (byte)((r + g + b) / 3);
+                    tmpImage[w, h] = new Rgba32(avg, avg, avg);
+                }
+            }
+
+            grayscaleImages.Add(tmpImage);
+            return grayscaleImages;
+        }
+
+        private static List<Image<Rgba32>> ConvertImageFromMinimunAlogorithm(Image<Rgba32> image)
+        {
+            List<Image<Rgba32>> grayscaleImages = new List<Image<Rgba32>>();
+            int imageWidth = image.Width;
+            int imageHeight = image.Height;
+
+            Image<Rgba32> tmpImage = new Image<Rgba32>(imageWidth, imageHeight);
+
+            for (int h = 0; h < imageHeight; h++)
+            {
+                for (int w = 0; w < imageWidth; w++)
+                {
+                    Rgba32 pixel = image[w, h];
+                    byte r = pixel.R;
+                    byte g = pixel.G;
+                    byte b = pixel.B;
+                    byte min = Math.Min(Math.Min(r, g), b);
+                    tmpImage[w, h] = new Rgba32(min, min, min);
+                }
+            }
+
+            grayscaleImages.Add(tmpImage);
+            return grayscaleImages;
+        }
+
+        private static List<Image<Rgba32>> ConvertImageFromMaximunAlogorithm(Image<Rgba32> image)
+        {
+            List<Image<Rgba32>> grayscaleImages = new List<Image<Rgba32>>();
+            int imageWidth = image.Width;
+            int imageHeight = image.Height;
+
+            Image<Rgba32> tmpImage = new Image<Rgba32>(imageWidth, imageHeight);
+
+            for (int h = 0; h < imageHeight; h++)
+            {
+                for (int w = 0; w < imageWidth; w++)
+                {
+                    Rgba32 pixel = image[w, h];
+                    byte r = pixel.R;
+                    byte g = pixel.G;
+                    byte b = pixel.B;
+                    byte max = Math.Max(Math.Max(r, g), b);
+                    tmpImage[w, h] = new Rgba32(max, max, max);
+                }
+            }
+
+            grayscaleImages.Add(tmpImage);
+            return grayscaleImages;
+        }
+
+        private static List<Image<Rgba32>> ConvertImageFromComponentAlgorithm(Image<Rgba32> image)
+        {
+            List<Image<Rgba32>> grayscaleImages = new List<Image<Rgba32>>();
+            int imageWidth = image.Width;
+            int imageHeight = image.Height;
+
+            Image<Rgba32> rComponentImage = new Image<Rgba32>(imageWidth, imageHeight);
+            Image<Rgba32> gComponentImage = new Image<Rgba32>(imageWidth, imageHeight);
+            Image<Rgba32> bComponentImage = new Image<Rgba32>(imageWidth, imageHeight);
+
+            for (int h = 0; h < imageHeight; h++)
+            {
+                for (int w = 0; w < imageWidth; w++)
+                {
+                    Rgba32 pixel = image[w, h];
+                    byte r = pixel.R;
+                    byte g = pixel.G;
+                    byte b = pixel.B;
                     rComponentImage[w, h] = new Rgba32(r, r, r);
-
-                    // G Component
                     gComponentImage[w, h] = new Rgba32(g, g, g);
-
-                    // B Component
                     bComponentImage[w, h] = new Rgba32(b, b, b);
                 }
             }
